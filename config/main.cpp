397a1c9068116d5113ef7dd705ee38bb5ad8768b
raphaelsoul <raphaelsoul@163.com>
2018-09-25 14:42:15 +0800

    update calling of boost::filesystem::current_path

--- config/main.cpp
+++ config/main.cpp
@@ -2,12 +2,20 @@
 // Created by raphaelsoul on 9/25/18.
 //
 #include <iostream>
+#include <boost/filesystem.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 
+using namespace boost;
+using namespace std;
+
 int main () {
+
+    boost::filesystem::path fullPath(boost::filesystem::current_path());
+    std::cout << "Current path is : " << fullPath << std::endl;
+
     boost::property_tree::ptree pt;
-    boost::property_tree::ini_parser::read_ini("/Users/raphaelsoul/Projects/cpp-tutorial/config/sample.conf", pt);
+    boost::property_tree::ini_parser::read_ini("../../config/sample.conf", pt);
 
     std::cout << pt.get<std::string>("basic.foobar") << std::endl;
     std::cout << pt.get<std::string>("advanced.lorem") << std::endl;
